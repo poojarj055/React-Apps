383f19e026462eb156f6becc32890bf5bae152ee
PoojaRaj <poojarj055@gmail.com>
2024-07-09 06:02:58 +0530

    Update UseWindowSize.jsx

--- ConceptualNotes/UseWindowSize.jsx
+++ ConceptualNotes/UseWindowSize.jsx
@@ -1,3 +1,9 @@
+// The useWindowSize hook is a useful for retrieving and tracking the dimensions of the browser window within a React component. 
+// It attaches an event listener to the “resize” event, ensuring that the size is updated dynamically whenever the window is resized. 
+// The hook returns the “size” object, enabling components to access and utilize the window dimensions for various purposes, 
+// such as responsive layout adjustments, conditional rendering, or calculations based on the available space.
+
+
 import {useEffect, useState} from "react";
 
 const useWindowSize = () => {
